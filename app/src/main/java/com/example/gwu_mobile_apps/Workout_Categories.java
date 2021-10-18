c05a55250845c94a2c0c979024a4c3f933b4d8df
Garrett Ramela <81193304+garrettramela@users.noreply.github.com>
2021-10-17 21:22:56 -0400

    Creating the second activity to add exercise categories to image buttons.

---
+++ app/src/main/java/com/example/gwu_mobile_apps/Workout_Categories.java
@@ -0,0 +1,14 @@
+package com.example.gwu_mobile_apps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Workout_Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_categories);
+    }
+}
